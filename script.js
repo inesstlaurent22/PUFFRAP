4393968e01447be0fcfb44960e7d5410d7d2b969
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-04-02 12:30:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ window.addEventListener("load", () => {
   document.body.style.opacity = "1";
   document.body.style.transition = "opacity 0.6s ease";
 });
+
+const slides = document.querySelector(".slides");
+const allSlides = document.querySelectorAll(".slide");
+
+let index = 0;
+let startX = 0;
+let isDown = false;
+
+/* ===== TOUCH ===== */
+
+slides.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+slides.addEventListener("touchend", (e) => {
+
+  const endX = e.changedTouches[0].clientX;
+  const diff = startX - endX;
+
+  if(diff > 50) index = Math.min(index + 1, allSlides.length - 1);
+  if(diff < -50) index = Math.max(index - 1, 0);
+
+  updateSlide();
+});
+
+/* ===== MOUSE DRAG ===== */
+
+slides.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.clientX;
+});
+
+slides.addEventListener("mouseup", (e) => {
+  if(!isDown) return;
+
+  const diff = startX - e.clientX;
+
+  if(diff > 50) index = Math.min(index + 1, allSlides.length - 1);
+  if(diff < -50) index = Math.max(index - 1, 0);
+
+  updateSlide();
+  isDown = false;
+});
+
+slides.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+/* ===== UPDATE ===== */
+
+function updateSlide(){
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
