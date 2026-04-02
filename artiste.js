ddc91155bcf641ec7146913241d9d756db266da0
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-04-02 04:34:20 +0200

    Update artiste.js

--- artiste.js
+++ artiste.js
@@ -27,3 +27,30 @@ for(let i = 1; i <= 31; i++){
 
   calendar.appendChild(day);
 }
+
+/* SNAP SCROLL (effet iOS) */
+
+const slider = document.querySelector(".services-slider");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => isDown = false);
+slider.addEventListener("mouseup", () => isDown = false);
+
+slider.addEventListener("mousemove", (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5;
+
+  slider.scrollLeft = scrollLeft - walk;
+});
