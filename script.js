f6b9b1da450e29f482dee9dfefb01e60afb516a3
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-04-10 07:58:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
+  apiKey: "AIzaSyA4IF_NqUVXXQxMWz3F1SM32NN5vLUpRoI",
+  authDomain: "puffrap-46658.firebaseapp.com",
+  projectId: "puffrap-46658",
+  storageBucket: "puffrap-46658.firebasestorage.app",
+  messagingSenderId: "217849878785",
+  appId: "1:217849878785:web:e4e7d90ae3b77a19e76300",
+  measurementId: "G-5LBQ1595QF"
 };
 
 const app = initializeApp(firebaseConfig);
