4e9c2df5c6710a22676717619aa135c2b6b0c0d2
inesstlaurent22 <inessaintlaurent@hotmail.com>
2026-04-10 07:55:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
-/* NAV */
-function goToApp(){
-  window.location.href = "application.html";
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-function goToMagazine(){
-  alert("Magazine bientôt disponible");
-}
+import {
+  getAuth,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-/* ================= SLIDER ================= */
+import {
+  getFirestore,
+  setDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const slides = document.querySelector(".slides");
-const allSlides = document.querySelectorAll(".slide");
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+};
 
-let index = 0;
-let startX = 0;
-
-/* TOUCH */
-slides.addEventListener("touchstart", e => {
-  startX = e.touches[0].clientX;
-});
-
-slides.addEventListener("touchend", e => {
-  const diff = startX - e.changedTouches[0].clientX;
-
-  if(diff > 50) index = Math.min(index + 1, allSlides.length - 1);
-  if(diff < -50) index = Math.max(index - 1, 0);
-
-  update();
-});
-
-/* MOUSE */
-let isDown = false;
-
-slides.addEventListener("mousedown", e => {
-  isDown = true;
-  startX = e.clientX;
-});
-
-slides.addEventListener("mouseup", e => {
-  if(!isDown) return;
-
-  const diff = startX - e.clientX;
-
-  if(diff > 50) index = Math.min(index + 1, allSlides.length - 1);
-  if(diff < -50) index = Math.max(index - 1, 0);
-
-  update();
-  isDown = false;
-});
-
-function update(){
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
